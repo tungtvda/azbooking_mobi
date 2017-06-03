d5c3ea6b4b6168043f1992d3053d2eb9f36946a2
tungtv <tungtv.soict@gmail.com>
2017-06-03 16:03:39 +0700

    tungtv upcode

--- view/default/menu.php
+++ view/default/menu.php
@@ -5,93 +5,93 @@ require_once DIR . '/common/locdautiengviet.php';
 function view_menu($data = array())
 {
     $asign = array();
-    $asign['email']=$data['config'][0]->Email;
-    $asign['Logo']=$data['config'][0]->Logo;
-    $asign['Name']=$data['config'][0]->Name;
-    $asign['Hotline']=$data['config'][0]->Hotline;
-
-    $asign['trangchu']=$data['menu'][0]->name;
-    $asign['tour']=$data['menu'][1]->name;
-    $asign['khachsan']=$data['menu'][2]->name;
-    $asign['tintuc']=$data['menu'][3]->name;
-    $asign['lienhe']=$data['menu'][4]->name;
-
-
-    // active menu
-    $asign['trangchu_mn'] = ($data['active'] == 'trangchu') ? 'current-menu-parent' : '';
-    $asign['tour_trong_nuoc_mn'] = ($data['active'] == 'tour_trong_nuoc') ? 'current-menu-parent' : '';
-    $asign['tour_nuoc_ngoai_mn'] = ($data['active'] == 'tour_nuoc_ngoai') ? 'current-menu-parent' : '';
-//    $asign['tour_mn'] = ($data['active'] == 'tour') ? 'current-menu-parent' : '';
-    $asign['khachsan_mn'] = ($data['active'] == 'khachsan') ? 'current-menu-parent' : '';
-    $asign['tintuc_mn'] = ($data['active'] == 'tintuc') ? 'current-menu-parent' : '';
-    $asign['lienhe_mn'] = ($data['active'] == 'lienhe') ? 'current-menu-parent' : '';
-
-    $asign['danhmuc_menu'] ='';
-    if(count($data['danhmuc_menu'])>0){
-        $asign['danhmuc_menu'] .='<ul class="sub-menu">';
-        foreach($data['danhmuc_menu'] as $row){
-            $link_dm1=link_dm_tour1($row);
-            $asign['danhmuc_menu'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
-            $data_danhmuc2=danhmuc_2_getByTop('','id!=1 and danhmuc1_id='.$row->id,'position asc');
-            if(count($data_danhmuc2)>0){
-                $asign['danhmuc_menu'] .='<ul class="sub-menu">';
-                foreach($data_danhmuc2 as $row2){
-                    $link_dm2=link_dm_tour2($row2,$row->name_url);
-                    $asign['danhmuc_menu'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
-                }
-                $asign['danhmuc_menu'] .='</ul>';
-            }
-            $asign['danhmuc_menu'] .='</li>';
-        }
-        $asign['danhmuc_menu'] .='</ul>';
-    }
-
-    $asign['danhmuc_menu_quocte'] ='';
-    if(count($data['danhmuc_menu_quocte'])>0){
-        $asign['danhmuc_menu_quocte'] .='<ul class="sub-menu">';
-        foreach($data['danhmuc_menu_quocte'] as $row){
-            $link_dm1=link_dm_tour1($row);
-            $asign['danhmuc_menu_quocte'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
-            $data_danhmuc2=danhmuc_2_getByTop('','id!=1 and danhmuc1_id='.$row->id,'position asc');
-            if(count($data_danhmuc2)>0){
-                $asign['danhmuc_menu_quocte'] .='<ul class="sub-menu">';
-                foreach($data_danhmuc2 as $row2){
-                    $link_dm2=link_dm_tour2($row2,$row->name_url,1);
-                    $asign['danhmuc_menu_quocte'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
-                }
-                $asign['danhmuc_menu_quocte'] .='</ul>';
-            }
-            $asign['danhmuc_menu_quocte'] .='</li>';
-        }
-        $asign['danhmuc_menu_quocte'] .='</ul>';
-    }
-
-    $asign['danhmuc_khachsan'] ='';
-    if(count($data['danhmuc_khachsan'])>0)
-    {
-//        $asign['danhmuc_khachsan'] = print_item('menu_item', $data['danhmuc_khachsan']);
-        $asign['danhmuc_khachsan'] .='<ul class="sub-menu">';
-        foreach($data['danhmuc_khachsan'] as $row){
-            $link_dm1=link_danhmuc_khachsan($row);
-            $asign['danhmuc_khachsan'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
-            $data_danhmuc2=danhmuc_khachsan_2_getByTop('','id!=1 and danhmuc_id='.$row->id,'position asc');
-            if(count($data_danhmuc2)>0){
-                $asign['danhmuc_khachsan'] .='<ul class="sub-menu">';
-                foreach($data_danhmuc2 as $row2){
-                    $link_dm2=link_danhmuc_khachsan_2($row2,$row->name_url);
-                    $asign['danhmuc_khachsan'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
-                }
-                $asign['danhmuc_khachsan'] .='</ul>';
-            }
-            $asign['danhmuc_khachsan'] .='</li>';
-        }
-        $asign['danhmuc_khachsan'] .='</ul>';
-    }
-
-    $asign['danhmuc_tintuc'] ='';
-    if(count($data['danhmuc_tintuc'])>0)
-    {
-        $asign['danhmuc_tintuc'] = print_item('menu_item', $data['danhmuc_tintuc']);
-    }
+//    $asign['email']=$data['config'][0]->Email;
+//    $asign['Logo']=$data['config'][0]->Logo;
+//    $asign['Name']=$data['config'][0]->Name;
+//    $asign['Hotline']=$data['config'][0]->Hotline;
+//
+//    $asign['trangchu']=$data['menu'][0]->name;
+//    $asign['tour']=$data['menu'][1]->name;
+//    $asign['khachsan']=$data['menu'][2]->name;
+//    $asign['tintuc']=$data['menu'][3]->name;
+//    $asign['lienhe']=$data['menu'][4]->name;
+//
+//
+//    // active menu
+//    $asign['trangchu_mn'] = ($data['active'] == 'trangchu') ? 'current-menu-parent' : '';
+//    $asign['tour_trong_nuoc_mn'] = ($data['active'] == 'tour_trong_nuoc') ? 'current-menu-parent' : '';
+//    $asign['tour_nuoc_ngoai_mn'] = ($data['active'] == 'tour_nuoc_ngoai') ? 'current-menu-parent' : '';
+////    $asign['tour_mn'] = ($data['active'] == 'tour') ? 'current-menu-parent' : '';
+//    $asign['khachsan_mn'] = ($data['active'] == 'khachsan') ? 'current-menu-parent' : '';
+//    $asign['tintuc_mn'] = ($data['active'] == 'tintuc') ? 'current-menu-parent' : '';
+//    $asign['lienhe_mn'] = ($data['active'] == 'lienhe') ? 'current-menu-parent' : '';
+//
+//    $asign['danhmuc_menu'] ='';
+//    if(count($data['danhmuc_menu'])>0){
+//        $asign['danhmuc_menu'] .='<ul class="sub-menu">';
+//        foreach($data['danhmuc_menu'] as $row){
+//            $link_dm1=link_dm_tour1($row);
+//            $asign['danhmuc_menu'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
+//            $data_danhmuc2=danhmuc_2_getByTop('','id!=1 and danhmuc1_id='.$row->id,'position asc');
+//            if(count($data_danhmuc2)>0){
+//                $asign['danhmuc_menu'] .='<ul class="sub-menu">';
+//                foreach($data_danhmuc2 as $row2){
+//                    $link_dm2=link_dm_tour2($row2,$row->name_url);
+//                    $asign['danhmuc_menu'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
+//                }
+//                $asign['danhmuc_menu'] .='</ul>';
+//            }
+//            $asign['danhmuc_menu'] .='</li>';
+//        }
+//        $asign['danhmuc_menu'] .='</ul>';
+//    }
+//
+//    $asign['danhmuc_menu_quocte'] ='';
+//    if(count($data['danhmuc_menu_quocte'])>0){
+//        $asign['danhmuc_menu_quocte'] .='<ul class="sub-menu">';
+//        foreach($data['danhmuc_menu_quocte'] as $row){
+//            $link_dm1=link_dm_tour1($row);
+//            $asign['danhmuc_menu_quocte'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
+//            $data_danhmuc2=danhmuc_2_getByTop('','id!=1 and danhmuc1_id='.$row->id,'position asc');
+//            if(count($data_danhmuc2)>0){
+//                $asign['danhmuc_menu_quocte'] .='<ul class="sub-menu">';
+//                foreach($data_danhmuc2 as $row2){
+//                    $link_dm2=link_dm_tour2($row2,$row->name_url,1);
+//                    $asign['danhmuc_menu_quocte'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
+//                }
+//                $asign['danhmuc_menu_quocte'] .='</ul>';
+//            }
+//            $asign['danhmuc_menu_quocte'] .='</li>';
+//        }
+//        $asign['danhmuc_menu_quocte'] .='</ul>';
+//    }
+//
+//    $asign['danhmuc_khachsan'] ='';
+//    if(count($data['danhmuc_khachsan'])>0)
+//    {
+////        $asign['danhmuc_khachsan'] = print_item('menu_item', $data['danhmuc_khachsan']);
+//        $asign['danhmuc_khachsan'] .='<ul class="sub-menu">';
+//        foreach($data['danhmuc_khachsan'] as $row){
+//            $link_dm1=link_danhmuc_khachsan($row);
+//            $asign['danhmuc_khachsan'] .='<li class="menu-item-has-children"><a href="'.$link_dm1.'">'.$row->name.'</a>';
+//            $data_danhmuc2=danhmuc_khachsan_2_getByTop('','id!=1 and danhmuc_id='.$row->id,'position asc');
+//            if(count($data_danhmuc2)>0){
+//                $asign['danhmuc_khachsan'] .='<ul class="sub-menu">';
+//                foreach($data_danhmuc2 as $row2){
+//                    $link_dm2=link_danhmuc_khachsan_2($row2,$row->name_url);
+//                    $asign['danhmuc_khachsan'] .='<li><a href="'.$link_dm2.'">'.$row2->name.'</a></li>';
+//                }
+//                $asign['danhmuc_khachsan'] .='</ul>';
+//            }
+//            $asign['danhmuc_khachsan'] .='</li>';
+//        }
+//        $asign['danhmuc_khachsan'] .='</ul>';
+//    }
+//
+//    $asign['danhmuc_tintuc'] ='';
+//    if(count($data['danhmuc_tintuc'])>0)
+//    {
+//        $asign['danhmuc_tintuc'] = print_item('menu_item', $data['danhmuc_tintuc']);
+//    }
     print_template($asign, 'menu');
 }
---
+++ view/default/template/header.tpl
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tour Classic With Detail 4 Columns &#8211; Travel Tour</title>
+    <meta name='robots' content='noindex,follow'/>
+    <link rel='dns-prefetch' href='//maps.google.com'/>
+    <link rel='dns-prefetch' href='//fonts.googleapis.com'/>
+    <link rel='dns-prefetch' href='//s.w.org'/>
+    <link rel="alternate" type="application/rss+xml" title="Travel Tour &raquo; Feed"
+          href="https://demo.goodlayers.com/traveltour/feed/"/>
+    <link rel="alternate" type="application/rss+xml" title="Travel Tour &raquo; Comments Feed"
+          href="https://demo.goodlayers.com/traveltour/comments/feed/"/>
+    <link rel='stylesheet' id='gdlr-core-page-builder-css'
+          href='{SITE-NAME}/view/default/themes/css/page-builder.css?ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='font-awesome-css'
+          href='{SITE-NAME}/view/default/themes/css/font-awesome.min.css?ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='elegant-icon-css'
+          href='{SITE-NAME}/view/default/themes/css/style.css?ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='tourmaster-style-css'
+          href='{SITE-NAME}/view/default/themes/css/tourmaster.css?ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='tourmaster-custom-style-css'
+          href='{SITE-NAME}/view/default/themes/css/tourmaster-style-custom.css?1495903102&#038;ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='traveltour-style-core-css'
+          href='{SITE-NAME}/view/default/themes/css/style-core.css?ver=4.7.5'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='traveltour-custom-style-css'
+          href='{SITE-NAME}/view/default/themes/css/traveltour-style-custom.css?1495645121&#038;ver=4.7.5'
+          type='text/css' media='all'/>
+    <script type='text/javascript'
+            src='{SITE-NAME}/view/default/themes/js/jquery.js?ver=1.12.4'></script>
+</head>
